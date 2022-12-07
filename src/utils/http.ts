64639647cbb7265a79d5a668583aab475f734cd8
HuaHoangThanhBao <baohua01234@gmail.com>
2022-12-07 17:40:33 +0700

    ready to deploy

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,10 +1,11 @@
 import axios, { AxiosInstance } from 'axios';
+import { baseURL } from '../contants';
 
 class Http {
   instance: AxiosInstance;
   constructor() {
     this.instance = axios.create({
-      baseURL: process.env.REACT_APP_HOST,
+      baseURL: baseURL,
       timeout: 10000,
     });
     this.instance.interceptors.response.use((config) => {
