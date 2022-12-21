b8b98c2fa408b4b48673c7e520789f8f27ce1b5b
HuaHoangThanhBao <baohua01234@gmail.com>
2022-12-21 21:29:27 +0700

    Turn old css files into scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import { DocumentList } from './stories/pages/DocumentList'
 import { MyDocument } from './stories/pages/MyDocument'
-import './App.css'
+import './App.scss'
 
 function App() {
   return (
