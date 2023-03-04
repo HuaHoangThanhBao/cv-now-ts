bc37e137d8df2e8a9d27bc5c91c3f9feeabe43c1
bao hua <baohua01234@gmail.com>
2023-03-04 21:42:45 +0700

    Test post message

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { useNavigate, useRoutes } from 'react-router-dom'
 import { DocumentList } from './stories/pages/DocumentList'
 import { MyDocument } from './stories/pages/MyDocument'
 import { LandingPage } from './stories/pages/LadingPage'
-import { useEffectOnce } from './hooks'
 import './App.scss'
 import { http } from './utils'
+import { useEffect } from 'react'
 
 function App() {
   const navigate = useNavigate()
@@ -22,28 +22,32 @@ function App() {
       element: <DocumentList />
     }
   ])
-  useEffectOnce(() => {
+
+  function handleMessages(message: any) {
+    const { from, event, data } = JSON.parse(message)
+    if (event === 'login') {
+      alert(`data: ${data}`)
+      const { email } = data
+      alert(`email: ${email}`)
+      http.get(`users/getuserbyemail/${email}`).then((res: any) => {
+        alert(`res: ${res}`)
+        alert(`res id: ${res.data._id}`)
+        navigate(`/my-documents/${res.data._id}`)
+      }).catch((e) => {
+        alert(`error getuserbyemail: ${e}`)
+      })
+    }
+  }
+
+  useEffect(() => {
     document.addEventListener("message", (event: any) => {
       const message = event.data;
-  
       handleMessages(message);
     })
-    function handleMessages(message: any) {
-      const { from, event, data } = JSON.parse(message)
-      if (event === 'login') {
-        alert(`data: ${data}`)
-        const { email } = data
-        alert(`email: ${email}`)
-        http.get(`users/getuserbyemail/${email}`).then((res: any) => {
-          alert(`res: ${res}`)
-          alert(`res id: ${res.data._id}`)
-          navigate(`/my-documents/${res.data._id}`)
-        }).catch((e) => {
-          alert(`error getuserbyemail: ${e}`)
-        })
-      }
+    return () => {
+      document.removeEventListener('message', handleMessages)
     }
-  })
+  }, [])
 
   return <div className="App">{elements}</div>
 }
