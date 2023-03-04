f6c8de5bd820a46ee0bb62e53027704afe7fd6c1
bao hua <baohua01234@gmail.com>
2023-03-04 22:04:20 +0700

    Test post message

--- src/hooks/useFetchDocumentFromParam.tsx
+++ src/hooks/useFetchDocumentFromParam.tsx
@@ -10,6 +10,7 @@ import { RootState, useAppDispatch } from '../store'
 import { resetBlockState, updateState } from '../stories/organisms/Block/block.slice'
 import { updateNoNeeds } from '../stories/organisms/Drag/drag.slice'
 import { getResume, resetResume } from '../stories/pages/DocumentList/documentList.slice'
+import { useDevice } from './useDevice'
 import { useEffectOnce } from './useEffectOnce'
 
 export const useFetchDocumentFromParam = () => {
@@ -17,16 +18,20 @@ export const useFetchDocumentFromParam = () => {
   const { documentId } = params
   const [isUpdated, setIsUpdated] = useState(false)
   const resume = useSelector((state: RootState) => state.document.resume)
+  const { device } = useDevice()
   const dispatch = useAppDispatch()
   const navigate = useNavigate()
 
   useEffectOnce(() => {
     const promise = dispatch(getResume({ documentId: documentId || '-1' }))
-    promise.unwrap().catch((error) => {
-      if (error.message.includes(HttpStatus.UNAUTHORIZED)) {
-        navigate('/')
-      }
-    })
+    alert(`documentId: ${documentId}`)
+    if (device !== 'mobile') {
+      promise.unwrap().catch((error) => {
+        if (error.message.includes(HttpStatus.UNAUTHORIZED)) {
+          navigate('/')
+        }
+      })
+    }
     return () => {
       dispatch(resetResume())
       dispatch(resetBlockState())
@@ -36,6 +41,7 @@ export const useFetchDocumentFromParam = () => {
 
   useEffect(() => {
     if (resume && resume._id !== '-1') {
+      alert(`resume: ${resume._id}`)
       setIsUpdated(false)
       dispatch(updateState(resume))
       dispatch(updateCurrentTemplate(resume.template.currentTemplate))
