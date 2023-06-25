1bc652ad4011e9691ce1a9ba0c3fdc4bb43f045d
baohua <baohua01234@gmail.com>
2023-06-25 13:23:41 +0700

    fix: render one column format

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useRoutes } from 'react-router-dom'
 import { DocumentList } from './stories/pages/DocumentList'
 import { MyDocument } from './stories/pages/MyDocument'
-import { LandingPage } from './stories/pages/LadingPage'
+import { LandingPage } from './stories/pages/LandingPage'
 import { useGoogleLogin } from './hooks'
 import './App.scss'
 
